b21c936f10d9c951a872fecbc1e01b0871380dc0
tomdevn90 <tomdevvn90@gmail.com>
2024-08-07 14:04:32 +0700

    update acf options

--- framework/acf-options.php
+++ framework/acf-options.php
@@ -1,30 +1,30 @@
 <?php
 
 /* Add Theme Options Page */
-function autoart_add_theme_options_page() {
+function influencers_add_theme_options_page() {
     if( function_exists('acf_add_options_page') ) {
         $option_page = acf_add_options_page(array(
-            'page_title'    => esc_html__('Theme Options', 'autoart'),
-            'menu_title'    => esc_html__('Theme Options', 'autoart'),
+            'page_title'    => esc_html__('Theme Options', 'influencers'),
+            'menu_title'    => esc_html__('Theme Options', 'influencers'),
             'menu_slug'     => 'theme-options-page',
             'capability'    => 'edit_posts',
             'redirect'      => false
       ));
     }
 }
-add_action('acf/init', 'autoart_add_theme_options_page');
+add_action('acf/init', 'influencers_add_theme_options_page');
 
-function autoart_acf_json_save_point( $path ) {
+function influencers_acf_json_save_point( $path ) {
 	// update path
 	$path = get_template_directory() . '/framework/acf-options';
 
 	// return
 	return $path;
 }
-add_filter( 'acf/settings/save_json', 'autoart_acf_json_save_point' );
+add_filter( 'acf/settings/save_json', 'influencers_acf_json_save_point' );
 
-function autoart_acf_json_load_point( $paths ) {
-	// reautoartve original path (optional)
+function influencers_acf_json_load_point( $paths ) {
+	// reinfluencersve original path (optional)
 	unset( $paths[0] );
 	// append path
 	$paths[] = get_template_directory() . '/framework/acf-options';
@@ -32,4 +32,4 @@ function autoart_acf_json_load_point( $paths ) {
 	// return
 	return $paths;
 }
-add_filter( 'acf/settings/load_json', 'autoart_acf_json_load_point' );
+add_filter( 'acf/settings/load_json', 'influencers_acf_json_load_point' );
